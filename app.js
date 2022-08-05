e891d5385ecf6a7a433ba40a33774e980f9c983a
Livingston-k <kaddulivington@gmail.com>
2022-08-05 08:54:43 +0300

    user signup

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const postRoutes = require('./routes/posts')
+const userRoutes = require('./routes/user')
 
 // MIDDLEWARES
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
     // ROUTES
 app.use('/posts', postRoutes)
+app.use('/users', userRoutes)
 
 app.get('/', (req, res) => {
     res.send({ 'msg': "User postman to consume the api" })
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,36 @@
 const models = require('../models')
+const Validator = require("fastest-validator");
+const v = new Validator();
+var bcrypt = require('bcryptjs');
 
 // MODELS
 const User = models.User
+
+// CREATE USER
+const signUp = (req, res) => {
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(req.body.password, salt);
+    const user = {
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+    }
+    const schema = {
+        name: { type: "string", optional: false, max: 300 },
+        email: { type: "string", max: 1000 },
+        password: { type: "string" }
+    };
+    const validation = v.validate(user, schema)
+    if (validation != true) {
+        return res.status(400).send(validation)
+    }
+    User.create(user).then((data) => {
+        res.status(200).send({ 'msg': 'User created successfully' })
+    }).catch((err) => {
+        res.status(500).send({ 'msg': 'Error creating user', 'error': err })
+    })
+}
+
+module.exports = {
+    signUp
+}
