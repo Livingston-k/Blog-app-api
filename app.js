cc2e313a0b636cd50f10a40eb9b99e59304dfcd0
Livingston-k <kaddulivington@gmail.com>
2022-08-08 02:30:22 +0300

    image upload

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require('express')
 const app = express()
 const postRoutes = require('./routes/posts')
 const userRoutes = require('./routes/user')
+const imagesRoutes = require('./routes/images')
 require("dotenv").config();
 
 // MIDDLEWARES
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use('/uploads', express.static('uploads'))
 
 // ROUTES
 app.use('/posts', postRoutes)
 app.use('/users', userRoutes)
+app.use('/images', imagesRoutes)
 
 // DEFAULT ROUTE
 app.get('/', (req, res) => {
--- helpers/image-uploader.js
+++ helpers/image-uploader.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
 })
 
 const fileFilter = (req, file, callback) => {
-    if (file.mimeType === "image/jpeg" || file.mimeType === "png") {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimetype === "image/jpg") {
         callback(null, true)
     } else {
         callback(new Error("Unsupported file"), false)
